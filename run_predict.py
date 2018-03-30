64417dd87474a2c2f236a24f834b4fe6948c2784
siliang <siliangzhangusc@gmail.com>
2018-03-29 17:33:01 -0700

    run predict script

--- run_predict.py
+++ run_predict.py
@@ -7,3 +7,32 @@ Created on Wed Mar 14 11:49:30 2018
 """
 
 #used to test the trained model
+
+import os
+import ConfigParser
+
+
+#config file to read from
+config = ConfigParser.RawConfigParser()
+config.readfp(open(r'./configuration.txt'))
+#===========================================
+#name of the experiment!!
+name_experiment = config.get('experiment name', 'name')
+
+
+
+#check if all of the folder exists
+if not os.path.exists('./Result/'+name_experiment):
+    print("please train the model at first, then predict")
+    exit()
+if not os.path.exists('./model/'+name_experiment+'_architecture.json'):
+    print("please train the model at first, then predict")
+    exit()
+    
+if not os.path.exists('./weights/' + name_experiment):
+    print("please train the model at first, then predict")
+    exit()
+
+# finally run the prediction
+print("\n2. Run the prediction on GPU (no nohup)")
+os.system('python ./src/retinaNN_predict.py')
